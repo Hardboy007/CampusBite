0dba662dc3425144cdbf4a7c1672542c056ea3d0
Hardboy007 <hardik77.aman@gmail.com>
2025-09-26 23:59:39 +0530

    added modals

--- Frontend Files/User Selection page/script.js
+++ Frontend Files/User Selection page/script.js
@@ -18,3 +18,49 @@ document.addEventListener("DOMContentLoaded", function () {   //jab pura html co
 
     observer.observe(mobileImage);      //mobile image ko observe krna shuru kro
 });
+
+
+// Open modal on "Continue as Customer"
+document.querySelector(".customer-btn").addEventListener("click", function(e) {
+    e.preventDefault();      //prevent form submit
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("loginModal").style.display = "block";
+});
+//close modal ❌
+function closeModal(){
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("loginModal").style.display = "none";
+}
+
+//login functionality
+function login(){
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const errorMsg = document.getElementById("loginError");
+
+    // Hide previous error
+    errorMsg.style.display = "none";
+
+    if(user === "" || pass === ""){
+        errorMsg.textContent = "Please enter both email and password.";
+        errorMsg.style.display = "block";
+        return;
+    }
+}
+
+
+//for eye toggle for password
+function togglePassword(){
+    const passwordInput = document.getElementById("password");  //password wala input field
+    const toggleIcon = document.querySelector(".toggle-password");  //eye icon ko pakdo
+
+    if(passwordInput.type === "password"){  //agar password type yaani hidden dots me hai
+        passwordInput.type = "text";    //input ka type text kr do
+        toggleIcon.classList.remove("fa-eye-slash");  //eye 
+        toggleIcon.classList.add("fa-eye");
+    }else{                                  //agar already show ho rha h to hide kr do
+        passwordInput.type = "password";
+        toggleIcon.classList.remove("fa-eye");
+        toggleIcon.classList.add("fa-eye-slash");
+    }
+}
