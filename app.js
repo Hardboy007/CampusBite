27f1eb3707e2604400bb32a0c4507dc7d3cbe6e2
Hardboy007 <hardik77.aman@gmail.com>
2025-11-29 12:23:35 +0530

    use dynamic port for deployment

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.get("/profile", (req, res) => {
 // });
 
 
-app.listen(8080,()=>{
-    console.log("server running");
-
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
